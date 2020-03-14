1316f17cc719a13b478647a6557c3cf18f97a87a
kirwa-KO <56740152+kirwa-KO@users.noreply.github.com>
2020-03-14 13:17:47 +0100

    new philosoph

--- philo_one.c
+++ philo_one.c
@@ -5,27 +5,84 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ibaali1 <ibaali1@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/13 20:09:43 by ibaali1           #+#    #+#             */
-/*   Updated: 2020/03/13 20:22:46 by ibaali1          ###   ########.fr       */
+/*   Created: 2020/03/14 11:09:07 by ibaali1           #+#    #+#             */
+/*   Updated: 2020/03/14 13:16:06 by ibaali1          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo_one.h"
 
-void	init_data(int argc, char **argv)
+void	init_philosopheres(int argc, char **argv, int nb_philo)
 {
-	number_of_philosophers = ft_atoi(argv[1]);
-	time_to_die = ft_atoi(argv[2]);
-	time_to_eat = ft_atoi(argv[3]);
-	time_to_sleep = ft_atoi(argv[4]);
-	if (argc == 6)
-		number_of_times_each_philosopher_must_eat = ft_atoi(argv[6]);
-	philo_thread = ()
+	int		i;
+
+	i = 0;
+	g_thread_id = (pthread_t*)malloc(sizeof(pthread_t) * nb_philo);
+	g_die_thread_id = (pthread_t*)malloc(sizeof(pthread_t) * nb_philo);
+	g_forks = (pthread_mutex_t*)malloc(sizeof(pthread_mutex_t) * nb_philo);
+	while (i < nb_philo)
+	{
+		g_philo_one[i].number_of_philosophers = ft_atoi(argv[1]);
+		g_philo_one[i].time_to_die = ft_atoi(argv[2]);
+		g_philo_one[i].time_to_eat = ft_atoi(argv[3]);
+		g_philo_one[i].tim_to_sleep = ft_atoi(argv[4]);
+		g_philo_one[i].left_fork = MIN(i, i + 1);
+		g_philo_one[i].right_fork = MAX(i, i + 1);
+		g_philo_one[i].nb_eat = 0;
+		g_philo_one[i].nb_philo = i;
+		if (argc == 6)
+			g_philo_one[i].number_of_times_each_philosopher_must_eat =
+			ft_atoi(argv[5]);
+		i++;
+	}
 }
 
 int		main(int argc, char **argv)
 {
-	if (argc != 5 && argc != 6)
+	int		nb_philo;
+	int		i;
+
+	if (argc != 6 && argc != 5)
 		return (-1);
-	init_data(argc, argv);
+	nb_philo = ft_atoi(argv[1]);
+	g_philo_one = (t_philo*)malloc(sizeof(t_philo) * nb_philo);
+	init_philosopheres(argc, argv, nb_philo);
+	i = 0;
+	while (i++ < nb_philo)
+		pthread_create(&(g_thread_id[i - 1]), NULL, (void*)philosophere,
+		&(g_philo_one[i - 1]));
+	i = 0;
+	while (i++ < nb_philo)
+		pthread_create(&(g_die_thread_id[i - 1]), NULL, (void*)die,
+		&(g_philo_one[i - 1]))
+}
+
+void	*philosophere(void *parametre)
+{
+	t_philo		*philo;
+
+	philo = parametre;
+	while (1)
+	{
+		msg_print((*philo).nb_philo, THINKING);
+		pthread_mutex_lock(&(g_forks[(*philo).left_fork]));
+		msg_print((*philo).nb_philo, FORK);
+		pthread_mutex_lock(&(g_forks[(*philo).right_fork]));
+		msg_print((*philo).nb_philo, FORK);
+		(*philo).state = EATING;
+		msg_print((*philo).nb_philo, EATING);
+		(*philo).last_eat = get_time_in_milisecond();
+		(*philo).nb_eat += 1;
+		usleep((*philo).time_to_eat * 1000);
+		msg_print((*philo).nb_philo, SLEEPING);
+		(*philo).state = SLEEPING;
+		usleep((*philo).tim_to_sleep * 1000);
+		(*philo).state = THINKING;
+	}
+	return (NULL);
+}
+
+void	*die(void parametre)
+{
+	
 }
--- philo_one.h
+++ philo_one.h
@@ -5,33 +5,56 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: ibaali1 <ibaali1@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/03/06 22:53:20 by ibaali            #+#    #+#             */
-/*   Updated: 2020/03/13 20:20:44 by ibaali1          ###   ########.fr       */
+/*   Created: 2020/03/14 11:09:11 by ibaali1           #+#    #+#             */
+/*   Updated: 2020/03/14 13:15:48 by ibaali1          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_ONE_H
-#define PHILO_ONE_H
+# define PHILO_ONE_H
 # include <unistd.h>
 # include <stdio.h>
 # include <pthread.h>
 # include <stdlib.h>
 # include <sys/time.h>
+# define THINKING 0
+# define EATING 1
+# define SLEEPING 2
+# define FORK 3
+# define DIE 4
+# define MIN(x, y) ((x) > (y) ? (y) : (x))
+# define MAX(x, y) ((x) < (y) ? (y) : (x))
 
-typedef		struct	s_philo
+typedef	struct		s_philo
 {
+	uint64_t	number_of_philosophers;
+	uint64_t	time_to_die;
+	uint64_t	time_to_eat;
+	uint64_t	tim_to_sleep;
+	uint64_t	number_of_times_each_philosopher_must_eat;
+	uint64_t	last_eat;
+	int			left_fork;
+	int			right_fork;
 	int			nb_eat;
-	uint16_t	last_time_eat;
+	int			nb_philo;
+	int			state;
 }					t_philo;
 
-pthread_t			*philo_thread;
-pthread_mutex_t		*forks;
-pthread_mutex_t		*print_message;
-uint16_t			time_to_eat;
-uint16_t			time_to_sleep;
-uint16_t			time_to_die;
-int					number_of_philosophers;
-int					number_of_times_each_philosopher_must_eat;
-t_philo				*philo_data;
+t_philo				*g_philo_one;
+uint64_t			g_time;
+pthread_t			*g_thread_id;
+pthread_t			*g_die_thread_id;
+pthread_mutex_t		*g_forks;
+pthread_mutex_t		*g_message;
+
+void		msg_print(int nb_philo, int state);
+uint64_t	get_time_in_milisecond(void);
+void		ft_strcat(char *dst, const char *src);
+void		ft_putstr_fd(char *s, int fd);
+int			ft_strlen(const char *s);
+char		*ft_itoa(uint64_t n);
+int			ft_atoi(const char *str);
+void		*philosophere(void *parametre);
+void		*die(void *parametre);
 
 #endif
